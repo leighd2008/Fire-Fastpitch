f3f026aa950873e937c891ee39f5e620fb57960c
leighd2008 <leighd2008@gmail.com>
2019-07-15 19:50:10 -0400

    regenerated GOOGLE_SHEETS_API_KEY

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 export default {
-  apiKey: "AIzaSyDXWgY-hoLwry2KZIUjanfTzzocVMslkus",
+  apiKey: `${process.env.GOOGLE_SHEETS_API_KEY}`,
   discoveryDocs: 
     ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
   spreadsheetId: "AKfycbzIRYN6LMa39uxsMMne0b3x6osEHOhT3OcFSv25Fvdrx35vAA"
