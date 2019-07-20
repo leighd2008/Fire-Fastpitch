6bbc432b7cf0e8541886280317acaee6b471f383
leighd2008 <leighd2008@gmail.com>
2019-07-20 11:23:50 -0400

    Updates

---
+++ precache-manifest.73f924ae4c3d1af2a8678ce142f026dd.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b9e4bd26debe4ebeedfa7f5af65295a",
+    "url": "/Fire-Fastpitch/index.html"
+  },
+  {
+    "revision": "16d6c2b850b5c764739b",
+    "url": "/Fire-Fastpitch/static/css/2.fa4f61c0.chunk.css"
+  },
+  {
+    "revision": "48b150903f801eaff332",
+    "url": "/Fire-Fastpitch/static/css/main.3455ebf5.chunk.css"
+  },
+  {
+    "revision": "16d6c2b850b5c764739b",
+    "url": "/Fire-Fastpitch/static/js/2.4118e0b6.chunk.js"
+  },
+  {
+    "revision": "48b150903f801eaff332",
+    "url": "/Fire-Fastpitch/static/js/main.f710a868.chunk.js"
+  },
+  {
+    "revision": "3152b995d14f65e4732d",
+    "url": "/Fire-Fastpitch/static/js/runtime~main.7901c819.js"
+  },
+  {
+    "revision": "1aed52a27b0530b392ce7781e5e9c341",
+    "url": "/Fire-Fastpitch/static/media/Fire_Logo.1aed52a2.jpg"
+  },
+  {
+    "revision": "1a319f765ee46297b7f279aa2fe2febc",
+    "url": "/Fire-Fastpitch/static/media/ash-background-beautiful-216640.1a319f76.png"
+  },
+  {
+    "revision": "f4105ff1dc140fafc018531aeb344f04",
+    "url": "/Fire-Fastpitch/static/media/tshirt.f4105ff1.jpg"
+  }
+]);
